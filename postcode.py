ee09503949d6e4a17747a2cd44fbc49038c00cc7
joppeel <jonatan.lehtinen@aalto.fi>
2016-07-07 12:37:17 +0300

    Method getMoreData fixed

--- postcode.py
+++ postcode.py
@@ -9,7 +9,7 @@ def getFromDataBase(code):
 	try:
 		mariadb_connection = mariadb.connect(user='root', password='pythontesti', database='postcodes')
 		cursor = mariadb_connection.cursor()
-		cursor.execute("SELECT postcode, year, operator, downloadMeasurements, averageDownload, topDownload, averageUpload, topUpload FROM testicsv WHERE postcode=%s", (code,))
+		cursor.execute("SELECT postcode, year, operator, downloadMeasurements, averageDownload, topDownload, uploadMeasurements, averageUpload, topUpload FROM correctpostcode WHERE postcode=%s", (code,))
 		mariadb_connection.close()
 		return list(cursor)
 	except:
@@ -24,18 +24,26 @@ def getTheBestOperator(code, csvFileName):
 	for row in data:
 		operators.append(row[2])
 	operators = set(operators)
-	for row in operators:
-		print (row)
-	operators = dict.fromkeys(operators,0.0)
-	print (operators["DNA"])
-	operators["DNA"] = operators["DNA"] + 2
-	operators["DNA"] = operators["DNA"] + 2
-	print (operators)
+	operatorsPoints = dict.fromkeys(operators,0.0)
 	data = convertDatasStringToIntOrFloat(data)
-	if hasEnoughMeasurements(data):
-		return calculateBestOperator(data)
+	dataFrom2016 = []
+	for row in data:
+		if row[1] == "2016":
+			dataFrom2016.append(row)
+	year = 2015
+	dataFrom2016 = getMoreData(data, operatorsPoints)
+	print(dataFrom2016[0])
+	
+	#while(not hasEnoughMeasurements(dataFrom2016) and year > 2012):
+	#	dataFrom2016 = getMoreData(data)
+	#	year -= 1
+		#print ("moikkamoi")
+	if hasEnoughMeasurements(dataFrom2016[0]):
+		print("moikkamoi")
+		return calculateBestOperator(dataFrom2016[0])
 	
 	else:
+		'''
 		data2 = getDataFromPostalCodeAndYear(code, "2015", csvFileName)
 		data2 = convertDatasStringToIntOrFloat(data2)
 		firstBest = calculateBestOperator(data)
@@ -44,7 +52,92 @@ def getTheBestOperator(code, csvFileName):
 			return max(data, key=itemgette(5))[2]
 		else: 
 			return getBestOperatorFromTwoYearByMeasurements(data, data2, firstBest, secondBest)
+		'''
+		return "Not enough measurements, couldn't determine the best operator."
 
+
+def getMoreData(data, operatorsPoints):
+	operatorsToAddData = []
+	updatedPoints = operatorsPoints
+	operatorsHavingEnoughData = []
+	data.sort(key=lambda x: (x[2], x[1]), reverse = True)
+	addedOperators = []
+	for row in data:
+		if row[2] not in addedOperators:
+			addedOperators.append(row[2])
+			if row[3] <= 50:			
+				operatorsToAddData.append(row)
+			elif row[1] == "2016" and row[3] > 50:
+				operatorsHavingEnoughData.append(row)
+				updatedPoints[row[2]] = 2
+			elif row[1] == "2015" and row[3] > 50:
+				operatorsHavingEnoughData.append(row)
+				updatedPoints[row[2]] = 1
+			elif row[1] == "2014" and row[3] > 50:
+				operatorsHavingEnoughData.append(row)
+				updatedPoints[row[2]] = 0.5
+			elif row[1] == "2013" and row[3] > 50:
+				operatorsHavingEnoughData.append(row)
+	
+		
+
+	#for key, group in groupby(data, lambda x: x[2]):
+			#print(key)
+			#print(max(group, key=itemgetter(1)))
+	#		for row in group:
+	#
+	'''
+	for row in data:
+		if row[3] <= 50 and row[1]=="2016":			
+			operatorsToAddData.append(row)
+		elif row[1] == "2016" and row[3] > 50:
+			operatorsHavingEnoughData.append(row)
+			updatedPoints[row[2]] = 2
+	'''
+	count = 0
+	operatorsInDict = {}
+	collected = []
+	year = 2015
+	if len(operatorsToAddData) == 0:
+		notEnoughData = False
+	else:
+		notEnoughData = True 
+	#for row in operatorsToAddData:
+		#print("asdf")		
+		#print(row)
+	while(notEnoughData and year > 2012):
+		count = 0
+		for operator in operatorsToAddData:
+			for row in data:
+				if row[1] == str(year) and row[2] == operator[2]:
+					collected.append(row)
+			
+			isNotAlreadyInList = True
+			for row in operatorsHavingEnoughData:
+				if operator[2] == row[2]:
+					isNotAlreadyInList = False
+			if collected:
+				weightedAverageData = calculateWeightedAverage(collected[0], operator)
+
+				if weightedAverageData[3] > 50 and isNotAlreadyInList:
+					operatorsHavingEnoughData.append(weightedAverageData)
+					if year == 2015:
+						updatedPoints[weightedAverageData[2]] = 1
+					elif year == 2014:
+						updatedPoints[weightedAverageData[2]] = 0.5			
+				else:
+					operatorsToAddData[count] = weightedAverageData
+
+			if year == 2013 and isNotAlreadyInList:
+				operatorsHavingEnoughData.append(operatorsToAddData[count])
+			collected = []
+			count += 1
+		year -= 1
+	#for row in operatorsToAddData:
+	#	print("asdf222")
+	#	print(row)
+			
+	return (operatorsHavingEnoughData, updatedPoints)
 
 def getBestOperatorFromTwoYearByMeasurements(data, data2, firstBest, secondBest):
 	firstMeasurements = 0
@@ -62,6 +155,25 @@ def getBestOperatorFromTwoYearByMeasurements(data, data2, firstBest, secondBest)
 		return secondBest
 	else:
 		return "Tie between" + firstBest + " and " + secondBest
+
+'''
+mittaustenmäärä1*latausmäärä1 + mittaustenmäärä2*latausmäärä2/(mittaustenmäärä1+mittaustenmäärä2)
+'''
+
+def calculateWeightedAverage(data1, data2):	
+	
+	#print(data1)
+	#print("asdf")
+	#print(data2)
+	if data1[1] != data2[1]:
+		weightedDownloadSpeed = (data1[3] * data1[4] + data2[3] * data2[4]) / (data1[3] + data2[3])
+		newData = list(data2)
+		newData[4] = weightedDownloadSpeed
+		newData[3] = data1[3] + data2[3]
+		#print(newData)
+		return tuple(newData)
+	else:
+		return data2
 
 
 def calculateBestOperator(data):
@@ -126,9 +238,6 @@ def hasMostMeasurementsAndHighestDownlink(data):
 
 
 
-				
-
-
 def addSpeedForOperators(data):
 	highestSpeed = max(data, key=itemgetter(6)) 
 	newData = []
@@ -153,8 +262,11 @@ def isBigDifferencesInMeasurements(data):
 def convertDatasStringToIntOrFloat(data):
 	converted = []
 	for row in data:
-		converted.append((row[0], row[1], row[2], int(row[3]), float(row[4]), float(row[5]), float(row[6]), float(row[7])))
-	return converted		
+		converted.append((row[0], row[1], row[2], int(row[3]), float(row[4]), float(row[5]), int(row[6]), float(row[7]), float(row[8])))	
+	return converted	
+
+
+	
 
 
 def getBestOperatorFromYearAndCode(year, code, csvFileName):
@@ -182,7 +294,7 @@ def getBestOperatorFromYearAndCode(year, code, csvFileName):
 
 def hasEnoughMeasurements(data):
 	if len(data) != 0:
-		return all(int(i[4]) >50 for i in data)
+		return all(int(i[3]) > 50 for i in data)
 	else:
 		return False
 
