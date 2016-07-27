1c95387c9fb21bdc74d227d9222844684a94f61f
LKolehm <lauri.kolehmainen@aalto.fi>
2016-07-27 10:21:27 +0300

    filteriä parannettu

--- getAverage.py
+++ getAverage.py
@@ -1,7 +1,7 @@
 import mysql.connector as mariadb
 from _datetime import date, datetime, timedelta
 import datetime
-#from drawgraph import drawGraph
+from drawgraph import drawGraph
 
 def getFromDataBase(time, key, location, typeOfLocation, lengthOfTime, operator):
       
@@ -16,11 +16,11 @@ def getFromDataBase(time, key, location, typeOfLocation, lengthOfTime, operator)
             if not key and not operator:
                 cursor.execute("SELECT startedAt, uid, latency, downlink, uplink, postalcode, networkoperator FROM otaniemitesti3 WHERE startedAt BETWEEN adddate(%s,%s) AND %s", (time,lengthOfTime, time,))
             elif key and not operator:
-                cursor.execute("SELECT startedAt, uid, latency, downlink, uplink, postalcode, networkoperator FROM otaniemitesti3 WHERE startedAt BETWEEN adddate(%s,%s) AND %s AND key = %s",(time,lengthOfTime,time,key,))
+                cursor.execute("SELECT startedAt, uid, latency, downlink, uplink, postalcode, networkoperator FROM otaniemitesti3 WHERE startedAt BETWEEN adddate(%s,%s) AND %s AND uid = %s",(time,lengthOfTime,time,key,))
             elif not key and operator:
                 cursor.execute("SELECT startedAt, uid, latency, downlink, uplink, postalcode, networkoperator FROM otaniemitesti3 WHERE startedAt BETWEEN adddate(%s,%s) AND %s AND networkoperator = %s",(time,lengthOfTime,time,operator,))
             else:
-                cursor.execute("SELECT startedAt, uid, latency, downlink, uplink, postalcode, networkoperator FROM otaniemitesti3 WHERE startedAt BETWEEN adddate(%s,%s) AND %s AND key = %s AND networkoperator = %s",(time,lengthOfTime,time,key,operator))
+                cursor.execute("SELECT startedAt, uid, latency, downlink, uplink, postalcode, networkoperator FROM otaniemitesti3 WHERE startedAt BETWEEN adddate(%s,%s) AND %s AND uid = %s AND networkoperator = %s",(time,lengthOfTime,time,key,operator))
 
         if typeOfLocation == 1:
             #Query for data 
@@ -28,22 +28,22 @@ def getFromDataBase(time, key, location, typeOfLocation, lengthOfTime, operator)
             if not key and not operator:
                 cursor.execute("SELECT startedAt, uid, latency, downlink, uplink, postalcode, networkoperator FROM otaniemitesti3 WHERE startedAt BETWEEN adddate(%s,%s) AND %s AND postalcode LIKE %s", (time,lengthOfTime, time,location,))
             elif key and not operator:
-                cursor.execute("SELECT startedAt, uid, latency, downlink, uplink, postalcode, networkoperator FROM otaniemitesti3 WHERE startedAt BETWEEN adddate(%s,%s) AND %s AND key = %s AND postalcode LIKE %s",(time,lengthOfTime,time,key,location,))
+                cursor.execute("SELECT startedAt, uid, latency, downlink, uplink, postalcode, networkoperator FROM otaniemitesti3 WHERE startedAt BETWEEN adddate(%s,%s) AND %s AND uid = %s AND postalcode LIKE %s",(time,lengthOfTime,time,key,location,))
             elif not key and operator:
                 cursor.execute("SELECT startedAt, uid, latency, downlink, uplink, postalcode, networkoperator FROM otaniemitesti3 WHERE startedAt BETWEEN adddate(%s,%s) AND %s AND networkoperator = %s AND postalcode LIKE %s",(time,lengthOfTime,time,operator,location))
             else:
-                cursor.execute("SELECT startedAt, uid, latency, downlink, uplink, postalcode, networkoperator FROM otaniemitesti3 WHERE startedAt BETWEEN adddate(%s,%s) AND %s AND key = %s AND networkoperator = %s AND postalcode LIKE %s",(time,lengthOfTime,time,key,operator,location))
+                cursor.execute("SELECT startedAt, uid, latency, downlink, uplink, postalcode, networkoperator FROM otaniemitesti3 WHERE startedAt BETWEEN adddate(%s,%s) AND %s AND uid = %s AND networkoperator = %s AND postalcode LIKE %s",(time,lengthOfTime,time,key,operator,location))
 
         if typeOfLocation == 2:
             #Select data from certain time window and city
             if not key and not operator:
                 cursor.execute("SELECT startedAt, uid, latency, downlink, uplink, postalcode, networkoperator FROM otaniemitesti3 WHERE startedAt BETWEEN adddate(%s,%s) AND %s AND county = %s", (time,lengthOfTime, time,location,))
             elif key and not operator:
-                cursor.execute("SELECT startedAt, uid, latency, downlink, uplink, postalcode, networkoperator FROM otaniemitesti3 WHERE startedAt BETWEEN adddate(%s,%s) AND %s AND key = %s AND county = %s",(time,lengthOfTime,time,key,location,))
+                cursor.execute("SELECT startedAt, uid, latency, downlink, uplink, postalcode, networkoperator FROM otaniemitesti3 WHERE startedAt BETWEEN adddate(%s,%s) AND %s AND uid = %s AND county = %s",(time,lengthOfTime,time,key,location,))
             elif not key and operator:
                 cursor.execute("SELECT startedAt, uid, latency, downlink, uplink, postalcode, networkoperator FROM otaniemitesti3 WHERE startedAt BETWEEN adddate(%s,%s) AND %s AND networkoperator = %s AND county = %s",(time,lengthOfTime,time,operator,location))
             else:
-                cursor.execute("SELECT startedAt, uid, latency, downlink, uplink, postalcode, networkoperator FROM otaniemitesti3 WHERE startedAt BETWEEN adddate(%s,%s) AND %s AND key = %s AND networkoperator = %s AND county = %s",(time,lengthOfTime,time,key,operator,location))
+                cursor.execute("SELECT startedAt, uid, latency, downlink, uplink, postalcode, networkoperator FROM otaniemitesti3 WHERE startedAt BETWEEN adddate(%s,%s) AND %s AND uid = %s AND networkoperator = %s AND county = %s",(time,lengthOfTime,time,key,operator,location))
             
         #More querys can be added here
         
@@ -69,7 +69,7 @@ def getAverages(time, key, location, typeOfLocation, weekly, fir, timeWindow, re
     # more to be added
     
     #timeWindow: how many days of data is fetched (if fir=true, this doesn't matter)
-    #resolution: when drawing the graphs, this tells how many hours are included into one average.
+    #resolution: averages are calculated for every [resolution] hours, values 1-24
     #sortOperators: true/false, whether separation of operators is desired.
     
     #some data alteration needed if key is postal code
@@ -89,7 +89,7 @@ def getAverages(time, key, location, typeOfLocation, weekly, fir, timeWindow, re
         data = calculateAveragesDaily(time, key, location, typeOfLocation, timeWindow, resolution, sortOperators)
     return data
 
-def getAveragesSortOperators(time, key, weekly, fir, location, typeOfLocation, timeWindow, resolution, sortOperators):   
+def getAveragesSortOperators(time, key, location, typeOfLocation, weekly, fir, timeWindow, resolution, sortOperators):   
     #This function is used if operator sorting is needed.
     allData = []
     for operator in ["Elisa","Sonera","DNA"]:
@@ -99,24 +99,22 @@ def getAveragesSortOperators(time, key, weekly, fir, location, typeOfLocation, t
             data = calculateAveragesWeekly(time, key, location, typeOfLocation, timeWindow, resolution, operator,0,0)
         else:
             data = calculateAveragesDaily(time, key, location, typeOfLocation, timeWindow, resolution, operator)
+            
         allData.append(data)
     return allData
     
 def getAveragesWeekFir(time, key, location, typeOfLocation, timeWindow, resolution, sortOperators):
     #This function calculates weekly averages, and also takes into account the previous 3 weeks 
     #as a simple FIR filter.
-    
     for i in range(1,5):
             
-        data = getFromDataBase(time, key, location, typeOfLocation, timeWindow,sortOperators)
+        data = getFromDataBase(time, key, location, typeOfLocation, -7,sortOperators)
         newData = [(item[0], item[1], item[2], item[3], item[4], item[5][:5], item[6]) for item in data]
         
         if i == 1:
             values = calculateAveragesWeekly(0,0,0,0,0,resolution,0,1,newData)
             for line in values:
-                line[2] = line[2] * 0.9
-                line[3] = line[3] * 0.9
-                line[4] = line[4] * 0.9
+                line[-1] += 1
             
         elif i == 2:
             temp = calculateAveragesWeekly(0,0,0,0,0,resolution,0,1,newData)
@@ -124,6 +122,8 @@ def getAveragesWeekFir(time, key, location, typeOfLocation, timeWindow, resoluti
                 values[line][2] += temp[line][2] * 0.05
                 values[line][3] += temp[line][3] * 0.05
                 values[line][4] += temp[line][4] * 0.05
+                values[line][5] += temp[line][5]
+                values[line][-1] += 0.05
             
         elif i == 3:
             temp = calculateAveragesWeekly(0,0,0,0,0,resolution,0,1,newData)
@@ -131,6 +131,8 @@ def getAveragesWeekFir(time, key, location, typeOfLocation, timeWindow, resoluti
                 values[line][2] += temp[line][2] * 0.025
                 values[line][3] += temp[line][3] * 0.025
                 values[line][4] += temp[line][4] * 0.025
+                values[line][5] += temp[line][5]
+                values[line][-1] += 0.025
             
         elif i == 4:
             temp = calculateAveragesWeekly(0,0,0,0,0,resolution,0,1,newData)
@@ -138,8 +140,18 @@ def getAveragesWeekFir(time, key, location, typeOfLocation, timeWindow, resoluti
                 values[line][2] += temp[line][2] * 0.025
                 values[line][3] += temp[line][3] * 0.025
                 values[line][4] += temp[line][4] * 0.025
+                values[line][5] += temp[line][5]
+                values[line][-1] += 0.025
                 
         time = time - timedelta(days = 7)
+    
+    for line in values:
+        if line[-1]:
+            line[2] = line[2] / line[-1]
+            line[3] = line[3] / line[-1]    
+            line[4] = line[4] / line[-1]
+        line.pop(-1)
+
     return values
 
 def calculateAveragesDaily(time,key,location, typeOfLocation,timeWindow,resolution,sortOperators):  
@@ -199,7 +211,6 @@ def calculateAveragesWeekly(time,key,location,typeOfLocation,timeWindow,resoluti
         data = getFromDataBase(time, key, location, typeOfLocation, timeWindow,sortOperators)
         data = [(item[0], item[1], item[2], item[3], item[4], item[5][:5], item[6]) for item in data]
         data.sort(key=lambda x: (x[0]))
-        
     averages = []
     for day in range(0,7):
         for hour in range(0,24):
@@ -245,11 +256,14 @@ def calculateAveragesWeekly(time,key,location,typeOfLocation,timeWindow,resoluti
     
     if not filter:
         averages = [i for i in averages if i[-1]]
+    else:
+        for line in averages:
+            line.append(float(0))
     return averages
 
 if __name__ == '__main__':
     #time = datetime.date object
-    time = datetime.date(2016,6,13)
+    time = datetime.date(2016,6,1)
     #key = uid or 0
     key = 0
     #location: city or postal code
@@ -260,16 +274,16 @@ if __name__ == '__main__':
     # 2 = db query by city
     typeOfLocation = 2
     #from how many days datais acquired
-    timeWindow = -28
+    timeWindow = -70
     #averages are calculated for every [resolution] hours, values 1-24
-    resolution = 2
+    resolution = 1
     #use fir calculation or not, true/false
-    fir = 0
+    fir = 1
     #sort by operators, true/false
-    sortOperators = 1
+    sortOperators = 0
     #weekly or daily averages, true=weekly/false=daily
     weekly = 1
-    
-    
     data = getAverages(time, key, location, typeOfLocation, weekly, fir, timeWindow, resolution, sortOperators)
     print(data)
+    
+    drawGraph(data)
